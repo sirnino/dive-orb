bac1a0b890d9dba68ac50feba086e4f713224bcf
packituz <pacohm94@gmail.com>
2022-12-27 16:32:13 -0600

    (fix) fixed permissions when downloading dive

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo Downloading dive "${DIVE_VERSION}"
 
-sudo wget "https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_amd64.tar.gz" -O - | tar -xz -C /usr/local/bin/
+sudo wget "https://github.com/wagoodman/dive/releases/dcownload/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_amd64.tar.gz" -O - | sudo tar -xz -C /usr/local/bin/
